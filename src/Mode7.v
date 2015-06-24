f0393b7a773123cba2048dd588003bca00e4122b
unknown <jerinaudo@WS05LAB07.ad.dc.uba.ar>
2015-06-24 13:21:51 -0300

    TOMEN EL CODIGO MANGA DE PUTOS

--- src/Mode7.v
+++ src/Mode7.v
@@ -19,28 +19,30 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Mode7
-#(
-	parameter entTabla=15,
-	salidaTabla=15,
-	tamanioColor = 8,
-	entradaImagen = 12
-)
-(
-    input clk,
-    input reset,
-    input [7:0] color,
-    output hsync,
-    output vsync,
-    output ptick,
-    output von,
-    output [7:0] rgb
-    );
+	#(
+		parameter
+		entTabla = 360,
+		salidaTabla = 16,
+		tamanioColor = 8,
+		entradaImagen = 4096
+	)
+	(
+		input clk,
+		input reset,
+		input [7:0] color,
+		input [7:0] offsetx,
+		output hsync,
+		output vsync,
+		output ptick,
+		output von,
+		output [7:0] rgb
+   );
 
 	wire [9:0] pixel_x, pixel_y;
 	reg [9:0] offsety, offsety_next;
 	reg [21:0] ctr_div, ctr_next;
 	wire video_on, pixel_tick;
-	wire [7:0] rgb_out;
+	reg [7:0] rgb_out;
 	reg [7:0] rgb_reg;
 	
 	//wire clkdiv;
@@ -49,11 +51,17 @@ module Mode7
 		.video_on(video_on), .p_tick(pixel_tick),
 		.pixel_x(pixel_x), .pixel_y(pixel_y));
 		
-		
-//	reg [15:0] sen, cos;
-	reg [salidaTabla-1:0] array_sen [entTabla-1:0];
-	reg [salidaTabla-1:0] array_cos [entTabla-1:0];
-	reg [tamanioColor-1:0] array_img [entradaImagen-1:0];
+	reg [15:0] sen, cos;
+	wire [8:0] angle;
+	reg [15:0] array_sen [359:0];
+	reg [15:0] array_cos [359:0];
+	reg [7:0] array_img [4095:0];
+	
+	initial begin
+		$readmemb("cos.bin", array_cos, 0, 359);
+		$readmemb("sin.bin", array_sen, 0, 359);
+		$readmemb("bitmap.bin", array_img, 0, 4095);
+	end
 		
 	always @(posedge clk)
 		if (pixel_tick)
@@ -75,16 +83,21 @@ module Mode7
 		begin
 			ctr_next = ctr_div;
 			offsety_next = offsety;
-			if (ctr_div == 22'd2097152)
-				begin
-					ctr_next = 0;
-					offsety_next = offsety + 1;
-				end
-			else
-				begin
-					ctr_next = ctr_div + 1;
-					offsety_next = offsety;
-				end
+			
+			rgb_out = 8'b00000000;
+			if(pixel_x < 64 + offsetx && pixel_y < 64) begin
+				rgb_out = array_img[pixel_x + pixel_y * 64];
+			end else if(pixel_x < 640 && pixel_y < 480) begin
+				rgb_out = 8'b11111111;
+			end
+						
+			/*if (ctr_div == 22'd2097152) begin
+				ctr_next = 0;
+				offsety_next = offsety + 1;
+			end else	begin
+				ctr_next = ctr_div + 1;
+				offsety_next = offsety;
+			end*/
 		end
 
 	assign rgb = rgb_reg;
