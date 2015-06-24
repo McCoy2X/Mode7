15f49386563cb58bffba9a5639c095145ec0d078
igleria <igleria@dc.uba.ar>
2015-06-24 11:28:27 -0300

    un par de scripts

---
+++ scripts/generar_bitmap.py
@@ -0,0 +1,23 @@
+from math import *
+
+def imprimir_pixel(bitmap_file,black):
+    if black:
+        bitmap_file.write('00000000 ')
+    else:
+        bitmap_file.write('11111111 ')
+
+def main():
+    bitmap_file = open('bitmap.txt', 'w')
+    fila = 64
+    columna = 64
+    black = False
+
+
+    for i in xrange(0,fila):
+        black = not(black)
+        for j in xrange(0,columna):
+            if j % 8 == 0:
+                black = not(black)
+            imprimir_pixel(bitmap_file, black)
+if __name__ == '__main__':
+    main()
---
+++ scripts/generar_sin_cos.py
@@ -0,0 +1,29 @@
+from math import *
+
+
+def toBinary(num):
+    integerPart = '%i000000%i' % ((0 if (num >= 0.0) else 1),int(abs(num)))
+    decimalPArt = ''
+
+    number = abs(num - int(num))
+    for i in xrange(0,8):
+        if number*2 > 1:
+            decimalPArt = decimalPArt + '1'
+        else:
+            decimalPArt = decimalPArt + '0'
+        number = abs(number*2 - int(number*2))
+
+    return integerPart + decimalPArt
+def main():
+    fsin = open('sin.v', 'w')
+    fcos = open('cos.v', 'w')    
+    for n in xrange(0,360):
+        nsin = sin(radians(n)) 
+        ncos = cos(radians(n))
+        fsin.write('9\'b%s : out = %s' % ('{:09b}'.format(n), toBinary(nsin)))
+        fsin.write('\n')
+        fcos.write('9\'b%s : out = %s' % ('{:09b}'.format(n), toBinary(ncos)))
+        fcos.write('\n')
+
+if __name__ == '__main__':
+    main()
