f914a59eaeca0ac40b98db67d9ef1bd607aa8758
igleria <igleria@dc.uba.ar>
2015-06-24 13:18:09 -0300

    bitmap

--- scripts/generar_bitmap.py
+++ scripts/generar_bitmap.py
@@ -2,9 +2,11 @@ from math import *
 
 def imprimir_pixel(bitmap_file,black):
     if black:
-        bitmap_file.write('00000000 ')
+        bitmap_file.write('00000000')
+        bitmap_file.write('\n')
     else:
-        bitmap_file.write('11111111 ')
+        bitmap_file.write('11111111')
+        bitmap_file.write('\n')
 
 def main():
     bitmap_file = open('bitmap.txt', 'w')
@@ -14,7 +16,8 @@ def main():
 
 
     for i in xrange(0,fila):
-        black = not(black)
+        if i % 8 == 0:
+                black = not(black)
         for j in xrange(0,columna):
             if j % 8 == 0:
                 black = not(black)
